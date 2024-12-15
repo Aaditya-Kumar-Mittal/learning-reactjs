4a4c16e99f09acd92b42c053ba9f3be2951fb9fe
Aaditya Kumar Mittal <adityakumarashish1909@gmail.com>
2024-12-15 19:39:39 +0530

    update: Deleted unecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,9 @@
 import React from "react";
 import "./App.css";
-import ReactBasicsMethodPropsParent from "./components/ReactBasicsMethodPropsParent";
-import ReactBasicsConditionalRendering1 from "./components/ReactBasicsConditionalRendering1";
-import ReactBasicsConditionalRendering2 from "./components/ReactBasicsConditionalRendering2";
-import ReactBasicsConditionalRendering3 from "./components/ReactBasicsConditionalRendering3";
-import ReactBasicsListRendering1 from "./components/ReactBasicsListRendering1";
-import ReactBasicsStylingStylesheets1 from "./components/ReactBasicsStylingStylesheets1";
-import ReactBasicsStylingInline1 from "./components/ReactBasicsStylingInline1";
-import ReactFormHandling1 from "./components/ReactFormHandling1";
-import ReactBasicsComponentLifeCycle from "./components/ReactBasicsComponentLifeCycle";
-import ReactFragements1 from "./components/ReactFragements1";
-import ReactComponentParentComponent1 from "./components/ReactComponentParentComponent1";
-import ReactComponentRefs1 from "./components/ReactComponentRefs1";
 
 function App() {
   return (
     <div className="App">
-      {/* <ReactBasicsMethodPropsParent /> */}
-      {/* <ReactBasicsConditionalRendering1/>
-       */}
-       {/* <ReactBasicsConditionalRendering2/> */}
-       {/* <ReactBasicsConditionalRendering3/> */}
-       {/* <ReactBasicsListRendering1/> */}
-       {/* <ReactBasicsStylingStylesheets1/>  */}
-       {/* <ReactBasicsStylingInline1/> */}
-       {/* <ReactFormHandling1/> */}
-       {/* <ReactBasicsComponentLifeCycle/> */}
-       {/* <ReactFragements1/> */}
-       {/* <ReactComponentParentComponent1/> */}
-       <ReactComponentRefs1/>
     </div>
   );
 }
