0c9df7e4cde7c0323194427def4710d6b8128aa9
Aaditya Kumar Mittal <adityakumarashish1909@gmail.com>
2024-12-28 17:41:25 +0530

    feat: Completed React Table Sorting

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 // import "./App.css";
 import ReactTableBasicTable from "./react-table/ReactTableBasicTable";
+import ReactTableHeaderGroups from "./react-table/ReactTableHeaderGroups";
+import ReactTableSortingTable from "./react-table/ReactTableSortingTable";
 
 function App() {
   return (
     <div className="App">
-      <ReactTableBasicTable />
+      {/* <ReactTableBasicTable /> */}
+      {/* <ReactTableHeaderGroups /> */}
+      <ReactTableSortingTable />
     </div>
   );
 }
