043f3899a1f3a28bfcff1f32e8f5105daec8fee6
Aaditya Kumar Mittal <adityakumarashish1909@gmail.com>
2024-12-28 17:16:55 +0530

    feat: Completed Basic React Table

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,11 @@
 import React from "react";
-import "./App.css";
-import ReactHookUseCallback1 from "./hooks/ReactHookUseCallback1";
-import ReactHookUseMemo1 from "./hooks/ReactHookUseMemo1";
-import ReactHookUseRef1 from "./hooks/ReactHookUseRef1";
-import ReactHookUseRef3Class from "./hooks/ReactHookUseRef3Class";
-import ReactHookUseRef2 from "./hooks/ReactHookUseRef2";
-import ReactCustomHook1DocTitle1 from "./hooks/ReactCustomHook1DocTitle1";
-import ReactCustomHook1DocTitle2 from "./hooks/ReactCustomHook1DocTitle2";
-import ReactCustomHook2Counter1 from "./hooks/ReactCustomHook2Counter1";
-import ReactCustomHook2Counter2 from "./hooks/ReactCustomHook2Counter2";
-import ReactCustomHook3UserForm1 from "./hooks/ReactCustomHook3UserForm1";
+// import "./App.css";
+import ReactTableBasicTable from "./react-table/ReactTableBasicTable";
 
 function App() {
   return (
     <div className="App">
-      {/* <ReactHookUseContext1 /> */}
-      {/* <ReactHookUseContext2 /> */}
-      {/* <ReactHookUseReducer1 /> */}
-      {/* <ReactHookUseReducer2/> */}
-      {/* <ReactHookUseReducer3/> */}
-      {/* <ReactHookUseReducer4 /> */}
-      {/* <ReactHookUseReducer5 /> */}
-      {/* <ReactHookUseReducer6 /> */}
-      {/* <ReactHookUseCallback1 /> */}
-      {/* <ReactHookUseMemo1 /> */}
-      {/* <ReactHookUseRef1 /> */}
-      {/* <ReactHookUseRef3Class /> */}
-      {/* <ReactHookUseRef2 /> */}
-      {/* <ReactCustomHook1DocTitle1 /> */}
-      {/* <ReactCustomHook1DocTitle2 /> */}
-      {/* <ReactCustomHook2Counter1 /> */}
-      {/* <ReactCustomHook2Counter2 /> */}
-      <ReactCustomHook3UserForm1 />
+      <ReactTableBasicTable />
     </div>
   );
 }
